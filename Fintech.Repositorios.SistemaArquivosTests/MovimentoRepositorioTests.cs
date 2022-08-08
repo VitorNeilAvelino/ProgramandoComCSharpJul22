9ee97bc01d9f6a5421cfd897687cb70f955ea7b6
Vitor Neil Avelino <avelino.vitor@gmail.com>
2022-08-08 20:59:04 -0300

    Testes para Inserir Movimento

---
+++ Fintech.Repositorios.SistemaArquivosTests/MovimentoRepositorioTests.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Fintech.Repositorios.SistemaArquivos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Fintech.Dominio;
+
+namespace Fintech.Repositorios.SistemaArquivos.Tests
+{
+    [TestClass()]
+    public class MovimentoRepositorioTests
+    {
+        [TestMethod()]
+        public void InserirTest()
+        {
+            var conta = new ContaCorrente(new Agencia { Numero = 123 }, 526, "1");
+            
+            var movimento = new Movimento(200, Operacao.Saque);
+            movimento.Conta = conta;
+            
+            var repositorio = new MovimentoRepositorio();
+            repositorio.Inserir(movimento);
+        }
+    }
+}
